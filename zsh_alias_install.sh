eab5ff38d2f6930b5c66fcededdfd98e0ecafe0c
elfgzp <741424975@qq.com>
2019-08-24 01:03:11 +0800

    Update docker-compose. Add zsh_alias_install.sh

---
+++ zsh_alias_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LGOPATH=$(PWD)
+echo "Current lgo path: $LGOPATH"
+echo "export LGOPATH=$LGOPATH" >> ~/.zshrc
+echo 'alias lgoup="cd $LGOPATH && ./up.sh && cd -"' >> ~/.zshrc
+echo 'alias lgodown="cd $LGOPATH && ./down.sh && cd -"' >> ~/.zshrc
+echo 'alias lgoshell="docker exec -it jupyter_jupyter_1 /usr/local/bin/jupyter console --kernel lgo"' >> ~/.zshrc
+echo 'alias lgobash="docker exec -it jupyter_jupyter_1 /bin/bash"' >> ~/.zshrc
+echo "alias lgoget='f() {echo \"lgo get \$@\" && docker exec -it jupyter_jupyter_1 /usr/local/go/bin/go get -u \$@};f'" >> ~/.zshrc
+echo "Add alias lgoup, lgodown, lgoshell, lgobash, lgoget"
