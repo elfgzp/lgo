885f1c6fcefa6c54bd8e42b27e1f81f2a1567e83
elfgzp <741424975@qq.com>
2019-08-24 10:37:57 +0800

    Update zsh_alias_install.sh

--- zsh_alias_install.sh
+++ zsh_alias_install.sh
@@ -3,8 +3,8 @@
 LGOPATH=$(PWD)
 echo "Current lgo path: $LGOPATH"
 echo "export LGOPATH=$LGOPATH" >> ~/.zshrc
-echo 'alias lgoup="cd $LGOPATH && ./up.sh && cd -"' >> ~/.zshrc
-echo 'alias lgodown="cd $LGOPATH && ./down.sh && cd -"' >> ~/.zshrc
+echo 'alias lgoup="cd $LGOPATH/docker/jupyter/ && ./up.sh && cd -"' >> ~/.zshrc
+echo 'alias lgodown="cd $LGOPATH/docker/jupyter/down.sh && ./down.sh && cd -"' >> ~/.zshrc
 echo 'alias lgoshell="docker exec -it jupyter_jupyter_1 /usr/local/bin/jupyter console --kernel lgo"' >> ~/.zshrc
 echo 'alias lgobash="docker exec -it jupyter_jupyter_1 /bin/bash"' >> ~/.zshrc
 echo "alias lgoget='f() {echo \"lgo get \$@\" && docker exec -it jupyter_jupyter_1 /usr/local/go/bin/go get -u \$@};f'" >> ~/.zshrc
